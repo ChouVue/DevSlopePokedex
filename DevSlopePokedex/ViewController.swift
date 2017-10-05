dfe9dfbc6f682efa4b71344f5ada3815f33692ef
ChouVue <30632978+ChouVue@users.noreply.github.com>
2017-10-05 14:13:42 -0500

    Video 91: Added Audio And Custom Font

--- DevSlopePokedex/ViewController.swift
+++ DevSlopePokedex/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 //UICollectionViewDelegate: Tell that this is the class (IN THIS FILE) is going to be the DELEGATE for the collection view
 
@@ -18,11 +19,61 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
     @IBOutlet weak var collection: UICollectionView!
     
-    override func viewDidLoad() {
+    var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
         collection.dataSource = self
         collection.delegate = self
+        
+        parsePokemonCSV()
+        initAudio()
+        
+    }
+    
+    func initAudio()
+    {
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+        
+        do
+        {
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+        }
+        catch let err as NSError
+        {
+            print(err.debugDescription)
+        }
+    }
+    
+    func parsePokemonCSV()
+    {
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
+        
+        do
+        {
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+            print(rows)
+            
+            for row in rows
+            {
+                let pokeId = Int(row["id"]!)!
+                let name = row["identifier"]!
+                
+                let poke = Pokemon(name: name, pokedexId: pokeId)
+                pokemon.append(poke)
+            }
+        }
+        catch let err as NSError
+        {
+            print(err.debugDescription)
+        }
         
     }
     
@@ -30,8 +81,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell
         {
-            let pokemon = Pokemon(name: "Pokemon", pokedexId: indexPath.row)
-            cell.configureCell(pokemon: pokemon)
+            let poke = pokemon[indexPath.row]
+            cell.configureCell(poke)
             
             return cell
         }
@@ -48,7 +99,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     {
-        return 30
+        return pokemon.count
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int
@@ -59,6 +110,20 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
     {
         return CGSize(width: 90, height: 90)
+    }
+    
+    @IBAction func musicBtnPressed(_ sender: UIButton)
+    {
+        if musicPlayer.isPlaying
+        {
+            musicPlayer.pause()
+            sender.alpha = 0.2
+        }
+        else
+        {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
     }
     
     
